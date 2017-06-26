8701f69d504a32cd3bd70b961dffb8c91c86d5de
Jim Lebonitte <jim.lebonitte@nielsen.com>
2017-06-25 21:43:24 -0400

    small changes

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -66,11 +66,11 @@ class Root extends Component {
                   technologies who has been fortunate to work alongside great
                   people throughout my career that taught me what I know today.
                   Currently I am working as a Full Stack Architect focused on
-                  Cloud-Based Architecture, UI/JavaScript (Angular and React),
-                  Big Data (Spark and Hadoop), Analytics and Client Facing UI
-                  Products. Please feel free to reach out as I am open to
-                  networking and consulting opportunities.
-
+                  Cloud Architecture (AWS, Azure, Joyent), JavaScript (Angular,
+                  React, Nodejs), DevOps (Docker, TravisCI, Jenkins), Mobile
+                  (Ionic, React Native) and
+                  Big Data (Java, Spark, Hadoop). Please feel free to reach out
+                  as I am open to networking and consulting opportunities.
                 </h3>
               </Col>
               <Col xs={1} />
@@ -91,26 +91,32 @@ class Root extends Component {
         <Row>
           <Col xs={12}>
             <Tabs onChange={this.handleChange} value={this.state.slideIndex}>
-              <Tab label="Software Stacks" value={0} />
+              <Tab label="Cloud Migrations" value={0} />
               <Tab label="UI / Mobile Architecture" value={1} />
               <Tab label="DevOps Consulting" value={2} />
-              <Tab label="Cloud Migrations" value={3} />
-              <Tab label="Developer Training" value={4} />
+              <Tab label="Software Stack" value={3} />
             </Tabs>
             <SwipeableViews
               index={this.state.slideIndex}
               onChangeIndex={this.handleChange}
             >
               <div className="tab-detail-div">
-                <h2>Software Stack Definition</h2>
+                <h2>Cloud Migrations</h2>
                 <p className="tab-detail-text">
-                  Defining a standard, end-to-end software stack is critical for
-                  your development team's productivity. I have experience across
-                  a wide variety of programming languages, architecture
-                  components, cloud providers, etc. I do not feel that this is a
-                  one size fits all decision and I can help analyze your product
-                  / business needs along with your current development talent to
-                  determine the best stack for you!
+                  I have a lot of experience in helping companies adopt Public
+                  Cloud utilizing both a lift-and-shift model as well
+                  as re-architecting applications. There are a lot of benefits
+                  for companies to re-architect their applications before moving
+                  to cloud
+                  such as serverless technology, resource auto-scaling, etc.
+                  that make it attractive for companies to utilize the public
+                  cloud especially around dynamic traffic levels and reducing
+                  the need to maintain complex architectures to handle typical
+                  scaling issues as your application becomes more popular.
+                  Utilizing some of these technologies you can enable your
+                  application to support hundreds to millions of users while
+                  scaling your infrastructure up and down to only pay for what
+                  you need at that time.
                 </p>
               </div>
               <div className="tab-detail-div">
@@ -143,9 +149,8 @@ class Root extends Component {
                   practices that fit how their organizations work together and
                   maximize their productivity. This goes from
                   how developers commit their code, which cloud provider(s) you
-                  are using, code/application deployment etc. On top of all of
-                  that it is critical that you
-                  keep your infrastructure secure. I specialize in a wide
+                  are using, code/application deployment, interface points
+                  between organizations, etc. I specialize in a wide
                   variety of tools such as Docker, Terraform, Ansible, etc. and
                   have experience handling deployments On-Premise and with the
                   following cloud providers AWS, Azure,
@@ -153,19 +158,15 @@ class Root extends Component {
                 </p>
               </div>
               <div className="tab-detail-div">
-                <h2>Cloud Migrations</h2>
+                <h2>Software Stack Definition</h2>
                 <p className="tab-detail-text">
-                  This is another example of a controllable tab. Remember, if
-                  you use controllable Tabs, you need to give all of your tabs
-                  values or else you wont be able to select them.
-                </p>
-              </div>
-              <div className="tab-detail-div">
-                <h2>UI Architecture</h2>
-                <p className="tab-detail-text">
-                  This is another example of a controllable tab. Remember, if
-                  you use controllable Tabs, you need to give all of your tabs
-                  values or else you wont be able to select them.
+                  Defining a standard, end-to-end software stack is critical for
+                  your development team's productivity. I have experience across
+                  a wide variety of programming languages, architecture
+                  components, cloud providers, etc. I do not feel that this is a
+                  one size fits all decision and I can help analyze your product
+                  / business needs along with your current development talent to
+                  determine the best stack for you!
                 </p>
               </div>
 
@@ -175,19 +176,17 @@ class Root extends Component {
         <Row center="xs">
           <Col xs={12} />
         </Row>
-        <Row className="section" center="xs">
+        <Row className="section grey-bg" center="xs">
           <Col xs={12}>
             <h2>
-              A few technologies I enjoy working with
+              This website / blog was built utilizing the following technologies
             </h2>
           </Col>
         </Row>
-        <Row center="xs">
+        <Row center="xs" className="grey-bg">
           <Col xs={1} />
           <Col xs={10}>
             <img src={aws} alt="Amazon Web Services" />
-            <img className="tech-img-pad" src={node} alt="NodeJS" />
-            <img className="tech-img-pad" src={docker} alt="Docker" />
             <img className="tech-img-pad" src={react} alt="React" />
           </Col>
 
