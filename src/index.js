200f6a765686899254e7a2de20db49ade2ea835e
James Lebonitte <jlebonitte@gmail.com>
2017-07-12 19:21:38 -0400

    Adding google analytics

--- src/index.js
+++ src/index.js
@@ -5,12 +5,20 @@ import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ViewState from './mobx/ViewState';
 import { Provider } from 'mobx-react';
+import ReactGA from 'react-ga';
 import './index.css';
 // Needed for onTouchTap / material-ui library
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
+function logPageView() {
+	ReactGA.set({ page: window.location.pathname + window.location.search });
+	ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 const viewStore = new ViewState();
+ReactGA.initialize('UA-102506154-1');
+logPageView();
 
 ReactDOM.render(
 	<Provider viewStore={viewStore}>
